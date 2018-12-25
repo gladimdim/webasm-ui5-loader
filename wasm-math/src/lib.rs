0347f454c558ced407be45fcde5c19b0b61f1c58
Dmytro Gladkyi <gladimdim@gmail.com>
2018-12-25 09:06:02 +0200

    Added wasm-math folder

---
+++ wasm-math/src/lib.rs
@@ -0,0 +1,27 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+pub fn add_two(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn multiply_by(a: i32, b: i32) -> i32 {
+    a * b
+}
