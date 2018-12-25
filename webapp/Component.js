ae49749003b27e753e995bf03a6f6711770a19fe
Dmytro Gladkyi <gladimdim@gmail.com>
2018-12-25 09:02:32 +0200

    Added wasm project

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,21 @@
 sap.ui.define(["sap/ui/core/UIComponent"], function(UIComponent){
-	"use strict";
-	return UIComponent.extend('sap.ui.demo.todo.Component', {
-		metadata: {
-			manifest: "json"
-		}
-	});
+    "use strict";
+
+    var tbl = new WebAssembly.Table({initial:2, element:"anyfunc"});
+    fetch('./wasm_math_bg.wasm')
+        .then(response =>
+              response.arrayBuffer()
+             )
+        .then(bytes => WebAssembly.instantiate(bytes, {env: tbl}))
+        .then(results => {
+            var instance = results.instance;
+            console.log(tbl);
+            console.log(Object.keys(instance.exports));
+        })
+        .catch(console.error);
+    return UIComponent.extend('sap.ui.demo.todo.Component', {
+	metadata: {
+	    manifest: "json"
+	}
+    });
 });
