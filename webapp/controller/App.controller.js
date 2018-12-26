af647c48e8f72ed75f34780bafec6718fb3330de
Dmytro Gladkyi <d.gladkyi@sap.com>
2018-12-26 11:45:37 +0000

    Added libwabt to convert from binary to text format.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,140 +1,172 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (Controller, JSONModel, Filter, FilterOperator) {
-	'use strict';
-	var oModel = null;
-	return Controller.extend('com.gladimdim.webasmloader.controller.App', {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, Filter, FilterOperator) {
+    'use strict';
+    var oModel = null;
+    var b = null;
+    return Controller.extend('com.gladimdim.webasmloader.controller.App', {
+        onInit: function() {
+            this.aSearchFilters = [];
+            this.aTabFilters = [];
 
-		onInit: function () {
-			this.aSearchFilters = [];
-			this.aTabFilters = [];
+            fetch('./wasm_math_bg.wasm')
+                .then(response =>
+                    response.arrayBuffer()
+                )
+                .then(bytes => {
+                    b = bytes;
+                    return WebAssembly.instantiate(bytes);
+                })
+                .then(results => {
+                    var instance = results.instance;
+                    var functions = {
+                        "wasmFunctions": Object.keys(instance.exports)
+                            .map(fnName => {
+                                return {
+                                    value: instance.exports[fnName],
+                                    name: fnName
+                                };
+                            })
+                    };
+                    oModel = new JSONModel(functions);
+                    this.getView()
+                        .setModel(oModel, "Functions");
 
-			fetch('./wasm_math_bg.wasm')
-				.then(response =>
-					response.arrayBuffer()
-				)
-				.then(bytes => WebAssembly.instantiate(bytes))
-				.then(results => {
-					var instance = results.instance;
-					var functions = {
-						"wasmFunctions": Object.keys(instance.exports).map(fnName => {
-							return {
-								value: instance.exports[fnName],
-								name: fnName
-							};
-						})
-					};
-					oModel = new JSONModel(functions);
-					this.getView().setModel(oModel, "Functions")
-				})
-				.catch(console.error);
-		},
+                    WabtModule()
+                        .then(function(wabt) {
+                            try {
+                                var module = wabt.readWasm(b, {
+                                    readDebugNames: true
+                                });
 
-		/**
-		 * Adds a new todo item to the bottom of the list.
-		 */
-		addTodo: function () {
-			var oModel = this.getView().getModel();
-			var aTodos = jQuery.extend(true, [], oModel.getProperty('/todos'));
+                                module.generateNames();
+                                module.applyNames();
 
-			aTodos.push({
-				title: oModel.getProperty('/newTodo'),
-				completed: false
-			});
+                                var result =
+                                    module.toText({
+                                        foldExprs: true,
+                                        inlineExport: true
+                                    });
+                                debugger;
+                            } catch (e) {
+                            	console.error(e);
+                            }
+                        });
+                })
+                .catch(console.error);
+        },
 
-			oModel.setProperty('/todos', aTodos);
-			oModel.setProperty('/newTodo', '');
-		},
+        /**
+         * Adds a new todo item to the bottom of the list.
+         */
+        addTodo: function() {
+            var oModel = this.getView()
+                .getModel();
+            var aTodos = jQuery.extend(true, [], oModel.getProperty('/todos'));
 
-		/**
-		 * Removes all completed items from the todo list.
-		 */
-		clearCompleted: function () {
-			var oModel = this.getView().getModel();
-			var aTodos = jQuery.extend(true, [], oModel.getProperty('/todos'));
+            aTodos.push({
+                title: oModel.getProperty('/newTodo'),
+                completed: false
+            });
 
-			var i = aTodos.length;
-			while (i--) {
-				var oTodo = aTodos[i];
-				if (oTodo.completed) {
-					aTodos.splice(i, 1);
-				}
-			}
+            oModel.setProperty('/todos', aTodos);
+            oModel.setProperty('/newTodo', '');
+        },
 
-			oModel.setProperty('/todos', aTodos);
-		},
+        /**
+         * Removes all completed items from the todo list.
+         */
+        clearCompleted: function() {
+            var oModel = this.getView()
+                .getModel();
+            var aTodos = jQuery.extend(true, [], oModel.getProperty('/todos'));
 
-		/**
-		 * Updates the number of items not yet completed
-		 */
-		updateItemsLeftCount: function () {
-			var oModel = this.getView().getModel();
-			var aTodos = oModel.getProperty('/todos') || [];
+            var i = aTodos.length;
+            while (i--) {
+                var oTodo = aTodos[i];
+                if (oTodo.completed) {
+                    aTodos.splice(i, 1);
+                }
+            }
 
-			var iItemsLeft = aTodos.filter(function (oTodo) {
-				return oTodo.completed !== true;
-			}).length;
+            oModel.setProperty('/todos', aTodos);
+        },
 
-			oModel.setProperty('/itemsLeftCount', iItemsLeft);
-		},
+        /**
+         * Updates the number of items not yet completed
+         */
+        updateItemsLeftCount: function() {
+            var oModel = this.getView()
+                .getModel();
+            var aTodos = oModel.getProperty('/todos') || [];
 
-		/**
-		 * Trigger search for specific items. The removal of items is disable as long as the search is used.
-		 * @param {sap.ui.base.Event} oEvent Input changed event
-		 */
-		onSearch: function (oEvent) {
-			var oModel = this.getView().getModel();
+            var iItemsLeft = aTodos.filter(function(oTodo) {
+                    return oTodo.completed !== true;
+                })
+                .length;
 
-			// First reset current filters
-			this.aSearchFilters = [];
+            oModel.setProperty('/itemsLeftCount', iItemsLeft);
+        },
 
-			// add filter for search
-			var sQuery = oEvent.getSource().getValue();
-			if (sQuery && sQuery.length > 0) {
-				oModel.setProperty('/itemsRemovable', false);
-				var filter = new Filter("title", FilterOperator.Contains, sQuery);
-				this.aSearchFilters.push(filter);
-			} else {
-				oModel.setProperty('/itemsRemovable', true);
-			}
+        /**
+         * Trigger search for specific items. The removal of items is disable as long as the search is used.
+         * @param {sap.ui.base.Event} oEvent Input changed event
+         */
+        onSearch: function(oEvent) {
+            var oModel = this.getView()
+                .getModel();
 
-			this._applyListFilters();
-		},
+            // First reset current filters
+            this.aSearchFilters = [];
 
-		onFilter: function (oEvent) {
+            // add filter for search
+            var sQuery = oEvent.getSource()
+                .getValue();
+            if (sQuery && sQuery.length > 0) {
+                oModel.setProperty('/itemsRemovable', false);
+                var filter = new Filter("title", FilterOperator.Contains, sQuery);
+                this.aSearchFilters.push(filter);
+            } else {
+                oModel.setProperty('/itemsRemovable', true);
+            }
 
-			// First reset current filters
-			this.aTabFilters = [];
+            this._applyListFilters();
+        },
 
-			// add filter for search
-			var sFilterKey = oEvent.getParameter("key");
+        onFilter: function(oEvent) {
 
-			// eslint-disable-line default-case
-			switch (sFilterKey) {
-			case "active":
-				this.aTabFilters.push(new Filter("completed", FilterOperator.EQ, false));
-				break;
-			case "completed":
-				this.aTabFilters.push(new Filter("completed", FilterOperator.EQ, true));
-				break;
-			case "all":
-			default:
-				// Don't use any filter
-			}
+            // First reset current filters
+            this.aTabFilters = [];
 
-			this._applyListFilters();
-		},
+            // add filter for search
+            var sFilterKey = oEvent.getParameter("key");
 
-		_applyListFilters: function () {
-			var oList = this.byId("todoList");
-			var oBinding = oList.getBinding("items");
+            // eslint-disable-line default-case
+            switch (sFilterKey) {
+                case "active":
+                    this.aTabFilters.push(new Filter("completed", FilterOperator.EQ, false));
+                    break;
+                case "completed":
+                    this.aTabFilters.push(new Filter("completed", FilterOperator.EQ, true));
+                    break;
+                case "all":
+                default:
+                    // Don't use any filter
+            }
 
-			oBinding.filter(this.aSearchFilters.concat(this.aTabFilters), "todos");
-		}
+            this._applyListFilters();
+        },
 
-	});
+        _applyListFilters: function() {
+            var oList = this.byId("todoList");
+            var oBinding = oList.getBinding("items");
+
+            oBinding.filter(this.aSearchFilters.concat(this.aTabFilters), "todos");
+        }
+
+    });
 
 });
